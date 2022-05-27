a44c9e216b269a5fe9c725dea799bfd97a297988
Campbell Brown <campbellbrown093@gmail.com>
2022-05-28 11:55:11 +1200

    Replacing tabs with spaces

--- src/taskapp.cpp
+++ src/taskapp.cpp
@@ -1,9 +1,9 @@
 #include "taskapp.h"
 
 TaskApp::TaskApp(QWidget* parent)
-	: QMainWindow(parent)
+    : QMainWindow(parent)
 {
-	m_ui.setupUi(this);
-	m_ui.tasksDetailsSplitter->setStretchFactor(0, 5);
-	m_ui.tasksDetailsSplitter->setStretchFactor(1, 2);
+    m_ui.setupUi(this);
+    m_ui.tasksDetailsSplitter->setStretchFactor(0, 5);
+    m_ui.tasksDetailsSplitter->setStretchFactor(1, 2);
 }
