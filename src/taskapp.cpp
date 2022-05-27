3e55cd75ea22ca48d20ef4d27b95dbe639e0d51c
Campbell Brown <campbellbrown093@gmail.com>
2022-05-28 09:31:11 +1200

    Dynamic resizing

--- src/taskapp.cpp
+++ src/taskapp.cpp
@@ -1,9 +1,9 @@
 #include "taskapp.h"
 
-TaskApp::TaskApp(QWidget *parent)
-    : QMainWindow(parent)
+TaskApp::TaskApp(QWidget* parent)
+	: QMainWindow(parent)
 {
-    m_ui.setupUi(this);
-    m_ui.taskListSplitter->setStretchFactor(0, 5);
-    m_ui.taskListSplitter->setStretchFactor(1, 1);
+	m_ui.setupUi(this);
+	m_ui.tasksSplitter->setStretchFactor(0, 5);
+	m_ui.tasksSplitter->setStretchFactor(1, 2);
 }
