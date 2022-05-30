2ef13ef9127375787395cea373b43a34ed9672b9
Campbell Brown <campbellbrown093@gmail.com>
2022-05-30 20:45:11 +1200

    #10 Example of adding items

--- src/gui/tasklistview.cpp
+++ src/gui/tasklistview.cpp
@@ -11,6 +11,38 @@ TaskListView::TaskListView(QWidget *parent)
 
     initTreeSections();
     initButtons();
+
+    auto temp = new QTreeWidgetItem();
+    temp->setText(0, "Incomplete");
+    m_ui.taskListTree->addTopLevelItem(temp);
+    m_ui.taskListTree->expandItem(temp);
+    temp->setFirstColumnSpanned(true);
+
+    auto temp2 = new QTreeWidgetItem();
+    temp2->setText(0, "foo");
+    temp2->setData(1, Qt::DisplayRole, 1);
+    temp->addChild(temp2);
+
+    auto temp3 = new QTreeWidgetItem();
+    temp3->setText(0, "bar");
+    temp3->setData(1, Qt::DisplayRole, 2);
+    temp->addChild(temp3);
+
+    auto temp4 = new QTreeWidgetItem();
+    temp4->setText(0, "Complete");
+    m_ui.taskListTree->addTopLevelItem(temp4);
+    m_ui.taskListTree->expandItem(temp4);
+    temp4->setFirstColumnSpanned(true);
+
+    auto temp5 = new QTreeWidgetItem();
+    temp5->setText(0, "hello");
+    temp5->setData(1, Qt::DisplayRole, 3);
+    temp4->addChild(temp5);
+
+    auto temp6 = new QTreeWidgetItem();
+    temp6->setText(0, "yo");
+    temp6->setData(1, Qt::DisplayRole, -1);
+    temp4->addChild(temp6);
 }
 
 void TaskListView::initTreeSections()
