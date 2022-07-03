425f352e329550253e7c896352470d0bcaa1c24d
Campbell Brown <campbellbrown093@gmail.com>
2022-07-03 15:45:29 +1200

    #21 Updated create task dialog unit tests

--- tests/dialogs/createtaskdlg/createtaskdlg.pro
+++ tests/dialogs/createtaskdlg/createtaskdlg.pro
@@ -12,10 +12,12 @@ SOURCES += \
 HEADERS += \
     $${SRCPATH}/dialogs/createtaskdlg.h \
     $${SRCPATH}/models/priority.h \
+    $${SRCPATH}/models/projectcollection.h \
 
 SOURCES += \
     $${SRCPATH}/dialogs/createtaskdlg.cpp \
     $${SRCPATH}/models/priority.cpp \
+    $${SRCPATH}/models/projectcollection.cpp \
 
 FORMS += \
     $${SRCPATH}/dialogs/createtaskdlg.ui \
--- tests/dialogs/createtaskdlg/createtaskdlgtest.cpp
+++ tests/dialogs/createtaskdlg/createtaskdlgtest.cpp
@@ -1,5 +1,6 @@
 #include "dialogs/createtaskdlg.h"
 #include "models/project.h"
+#include "models/projectcollection.h"
 #include "models/task.h"
 #include <memory>
 #include <QRegularExpression>
@@ -19,26 +20,31 @@ private slots:
 
 private:
     Task m_task;
+    std::shared_ptr<ProjectCollection> m_projectCollection;
     std::unique_ptr<CreateTaskDlg> m_createTaskDlg;
 };
 
 void CreateTaskDlgTest::init()
 {
     m_task = Task();
-    QList<Project> projects;
+    m_projectCollection.reset(new ProjectCollection());
     Project proj1;
     Project proj2;
     proj1.name = "project foo";
+    proj1.uuid = QUuid("00000000-0000-0000-0000-000000000001");
     proj2.name = "project bar";
-    projects.append(proj1);
-    projects.append(proj2);
-    m_createTaskDlg.reset(new CreateTaskDlg(m_task, projects));
+    proj2.uuid = QUuid("00000000-0000-0000-0000-000000000002");
+    m_projectCollection->insert(0, proj1);
+    m_projectCollection->insert(0, proj2);
+    m_createTaskDlg.reset(new CreateTaskDlg(m_task, *m_projectCollection));
 }
 
 void CreateTaskDlgTest::check_comboBoxes_arePopulated()
 {
     QCOMPARE(m_createTaskDlg->m_ui.projectComboBox->count(), 2);
     QCOMPARE(m_createTaskDlg->m_ui.priorityComboBox->count(), Priority::Count);
+    QCOMPARE(m_createTaskDlg->m_ui.projectComboBox->currentIndex(), -1);
+    QCOMPARE(m_createTaskDlg->m_ui.priorityComboBox->currentIndex(), -1);
 }
 
 void CreateTaskDlgTest::check_savesOnAccept()
@@ -46,6 +52,7 @@ void CreateTaskDlgTest::check_savesOnAccept()
     // Given:
     m_createTaskDlg->m_ui.titleLineEdit->setText("someTitle");
     m_createTaskDlg->m_ui.notesPlainTextEdit->setPlainText("some\nNotes");
+    m_createTaskDlg->m_ui.projectComboBox->setCurrentIndex(0);
     m_createTaskDlg->m_ui.priorityComboBox->setCurrentIndex(Priority::High);
 
     // When:
@@ -54,6 +61,7 @@ void CreateTaskDlgTest::check_savesOnAccept()
     // Then:
     QCOMPARE(m_task.title, "someTitle");
     QCOMPARE(m_task.notes, "some\nNotes");
+    QCOMPARE(m_task.projectId, QUuid("00000000-0000-0000-0000-000000000002"));
     auto regex = QRegularExpression("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}");
     QCOMPARE(regex.match(m_task.uuid.toString(QUuid::WithoutBraces)).hasMatch(), true);
     QCOMPARE(m_task.priority, Priority::High);
@@ -70,7 +78,8 @@ void CreateTaskDlgTest::check_preventEmptyTitle()
     // Then:
     QCOMPARE(m_task.title, "");
     QCOMPARE(m_task.notes, "");
-    QCOMPARE(m_task.uuid.toString(QUuid::WithoutBraces), "00000000-0000-0000-0000-000000000000");
+    QCOMPARE(m_task.projectId, QUuid());
+    QCOMPARE(m_task.uuid, QUuid());
     QCOMPARE(m_task.timeCreated, QDateTime());
     QCOMPARE(m_task.priority, Priority::Medium);
 }
@@ -87,7 +96,8 @@ void CreateTaskDlgTest::check_preventEmptyPriority()
     // Then:
     QCOMPARE(m_task.title, "");
     QCOMPARE(m_task.notes, "");
-    QCOMPARE(m_task.uuid.toString(QUuid::WithoutBraces), "00000000-0000-0000-0000-000000000000");
+    QCOMPARE(m_task.projectId, QUuid());
+    QCOMPARE(m_task.uuid, QUuid());
     QCOMPARE(m_task.timeCreated, QDateTime());
     QCOMPARE(m_task.priority, Priority::Medium);
 }
@@ -104,7 +114,8 @@ void CreateTaskDlgTest::check_doesntSaveTaskOnReject()
     // Then:
     QCOMPARE(m_task.title, "");
     QCOMPARE(m_task.notes, "");
-    QCOMPARE(m_task.uuid.toString(QUuid::WithoutBraces), "00000000-0000-0000-0000-000000000000");
+    QCOMPARE(m_task.projectId, QUuid());
+    QCOMPARE(m_task.uuid, QUuid());
     QCOMPARE(m_task.timeCreated, QDateTime());
     QCOMPARE(m_task.priority, Priority::Medium);
 }
