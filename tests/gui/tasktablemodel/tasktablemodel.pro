a04031132993e264c18544f4f76a30f3afec0ef9
Campbell Brown <campbellbrown093@gmail.com>
2022-07-04 17:27:22 +1200

    #21 Task table model unit tests

--- tests/gui/tasktablemodel/tasktablemodel.pro
+++ tests/gui/tasktablemodel/tasktablemodel.pro
@@ -12,7 +12,9 @@ SOURCES += \
 HEADERS += \
     $${SRCPATH}/gui/tasktablemodel.h \
     $${SRCPATH}/models/priority.h \
+    $${SRCPATH}/models/projectcollection.h \
 
 SOURCES += \
     $${SRCPATH}/gui/tasktablemodel.cpp \
     $${SRCPATH}/models/priority.cpp \
+    $${SRCPATH}/models/projectcollection.cpp \
--- tests/gui/tasktablemodel/tasktablemodeltest.cpp
+++ tests/gui/tasktablemodel/tasktablemodeltest.cpp
@@ -1,4 +1,5 @@
 #include "gui/tasktablemodel.h"
+#include "models/projectcollection.h"
 #include <QDateTime>
 #include <QFont>
 #include <QtTest/QtTest>
@@ -19,15 +20,19 @@ private slots:
     void check_removeTask_all();
 
 private:
-    void addCustomTask(QString title, Priority priority, QDateTime timeCreated);
+    void addCustomTask(QString title, Priority priority, QUuid projectId, QDateTime timeCreated);
 
 private:
+    QList<Task> m_tasks;
+    std::shared_ptr<ProjectCollection> m_projectCollection;
     std::unique_ptr<TaskTableModel> m_taskTableModel;
 };
 
 void TaskTableModelTest::init()
 {
-    m_taskTableModel.reset(new TaskTableModel());
+    m_tasks.clear();
+    m_projectCollection.reset(new ProjectCollection());
+    m_taskTableModel.reset(new TaskTableModel(m_tasks, *m_projectCollection));
 }
 
 void TaskTableModelTest::check_onStartup()
@@ -45,9 +50,11 @@ void TaskTableModelTest::check_headerData_data()
     QTest::addColumn<QString>("expectedHeader");
     auto titleIdx = (int)TaskTableModel::Title;
     auto priorityIdx = (int)TaskTableModel::Priority;
+    auto projectIdx = (int)TaskTableModel::ProjectCol;
     auto timeCreatedIdx = (int)TaskTableModel::TimeCreated;
     QTest::addRow("title") << titleIdx << m_taskTableModel->m_headerNames[titleIdx];
     QTest::addRow("priority") << priorityIdx << m_taskTableModel->m_headerNames[priorityIdx];
+    QTest::addRow("projectCol") << projectIdx << m_taskTableModel->m_headerNames[projectIdx];
     QTest::addRow("time created") << timeCreatedIdx << m_taskTableModel->m_headerNames[timeCreatedIdx];
 }
 
@@ -68,10 +75,15 @@ void TaskTableModelTest::check_headerData()
     QCOMPARE(textAlignment, int(Qt::AlignLeft | Qt::AlignVCenter));
 }
 
-void TaskTableModelTest::addCustomTask(QString title, Priority priority, QDateTime timeCreated)
+void TaskTableModelTest::addCustomTask(
+    QString title,
+    Priority priority,
+    QUuid projectId,
+    QDateTime timeCreated)
 {
     Task task;
     task.title = title;
+    task.projectId = projectId;
     task.priority = priority;
     task.timeCreated = timeCreated;
     m_taskTableModel->addTask(task);
@@ -82,9 +94,18 @@ void TaskTableModelTest::check_addTask()
     // Given:
     QCOMPARE(m_taskTableModel->rowCount(), 0);
     QCOMPARE(m_taskTableModel->m_tasks.count(), 0);
+    Project proj;
+    proj.name = "projectA";
+    proj.uuid = QUuid("00000000-0000-0000-0000-000123456789");
+    m_projectCollection->insert(0, proj);
 
     // When:
-    addCustomTask("taskTitle", Priority::High, QDateTime::fromSecsSinceEpoch(10921, Qt::UTC, 0));
+    addCustomTask(
+        "taskTitle",
+        Priority::High,
+        proj.uuid,
+        QDateTime::fromSecsSinceEpoch(10921, Qt::UTC, 0)
+    );
 
     // Then:
     QCOMPARE(m_taskTableModel->rowCount(), 1);
@@ -92,12 +113,15 @@ void TaskTableModelTest::check_addTask()
 
     auto firstRowTitle = m_taskTableModel->index(0, TaskTableModel::Title);
     auto firstRowPriority = m_taskTableModel->index(0, TaskTableModel::Priority);
+    auto firstRowProject = m_taskTableModel->index(0, TaskTableModel::ProjectCol);
     auto firstRowTimeCreated = m_taskTableModel->index(0, TaskTableModel::TimeCreated);
     QCOMPARE(m_taskTableModel->data(firstRowTitle, Qt::DisplayRole).toString(), "taskTitle");
     QCOMPARE(m_taskTableModel->data(firstRowTitle, Qt::UserRole).toString(), "taskTitle");
     QCOMPARE(m_taskTableModel->data(firstRowPriority, Qt::DisplayRole).toString(),
              PriorityDisplay::text(Priority::High));
     QCOMPARE(m_taskTableModel->data(firstRowPriority, Qt::UserRole).toInt(), Priority::High);
+    QCOMPARE(m_taskTableModel->data(firstRowProject, Qt::UserRole).toString(), "projectA");
+    QCOMPARE(m_taskTableModel->data(firstRowProject, Qt::DisplayRole).toString(), "projectA");
     QCOMPARE(m_taskTableModel->data(firstRowTimeCreated, Qt::DisplayRole).toString(), "01/01/70 03:02:01 am");
     QCOMPARE(m_taskTableModel->data(firstRowTimeCreated, Qt::UserRole).toDateTime().date(), QDate(1970, 1, 1));
     QCOMPARE(m_taskTableModel->data(firstRowTimeCreated, Qt::UserRole).toDateTime().time(), QTime(3, 2, 1));
@@ -106,9 +130,9 @@ void TaskTableModelTest::check_addTask()
 void TaskTableModelTest::check_removeTask_beginning()
 {
     // Given:
-    addCustomTask("task1", Priority::Low, QDateTime());
-    addCustomTask("task2", Priority::Low, QDateTime());
-    addCustomTask("task3", Priority::Low, QDateTime());
+    addCustomTask("task1", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task2", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task3", Priority::Low, QUuid(), QDateTime());
 
     // When:
     m_taskTableModel->removeTaskAt(0);
@@ -125,9 +149,9 @@ void TaskTableModelTest::check_removeTask_beginning()
 void TaskTableModelTest::check_removeTask_middle()
 {
     // Given:
-    addCustomTask("task1", Priority::Low, QDateTime());
-    addCustomTask("task2", Priority::Low, QDateTime());
-    addCustomTask("task3", Priority::Low, QDateTime());
+    addCustomTask("task1", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task2", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task3", Priority::Low, QUuid(), QDateTime());
 
     // When:
     m_taskTableModel->removeTaskAt(1);
@@ -144,9 +168,9 @@ void TaskTableModelTest::check_removeTask_middle()
 void TaskTableModelTest::check_removeTask_end()
 {
     // Given:
-    addCustomTask("task1", Priority::Low, QDateTime());
-    addCustomTask("task2", Priority::Low, QDateTime());
-    addCustomTask("task3", Priority::Low, QDateTime());
+    addCustomTask("task1", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task2", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task3", Priority::Low, QUuid(), QDateTime());
 
     // When:
     m_taskTableModel->removeTaskAt(2);
@@ -163,9 +187,9 @@ void TaskTableModelTest::check_removeTask_end()
 void TaskTableModelTest::check_removeTask_all()
 {
     // Given:
-    addCustomTask("task1", Priority::Low, QDateTime());
-    addCustomTask("task2", Priority::Low, QDateTime());
-    addCustomTask("task3", Priority::Low, QDateTime());
+    addCustomTask("task1", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task2", Priority::Low, QUuid(), QDateTime());
+    addCustomTask("task3", Priority::Low, QUuid(), QDateTime());
 
     // When:
     m_taskTableModel->removeTaskAt(0);
